059ef109285abce1aeba05987e5b514fed3488fb
BeckyReed <reed.b.r.m@gmail.com>
2023-09-24 17:19:12 -0400

    Add to localstroage arrays w/ class name as key

--- exam_analysis/src/scripts/FileHandling.js
+++ exam_analysis/src/scripts/FileHandling.js
@@ -5,8 +5,6 @@ async function handleFileAsync() {
 
     const file = document.getElementById('inputFile').files;
 
-
-
     let reader = new FileReader();
     reader.readAsArrayBuffer(file[0]);
     reader.onload = function(e) {
@@ -21,9 +19,51 @@ async function handleFileAsync() {
         const jsonData = utils.sheet_to_json(worksheet, {defval:""});
     
         console.log(JSON.stringify(jsonData));
+
+        dataByClassToLocal(jsonData);
     }
 
 
+    //CLEAR LOCAL STORAGE FOR TEST
+    localStorage.clear();
+}
+
+function dataByClassToLocal(jsonData) {
+
+    //localStorage.setItem('test', []);
+
+    jsonData.forEach(element => {
+        console.log(element);
+        //let data = JSON.parse(element);
+        let className = "" + element.Course_Number+element.Semester+element.Year;
+        console.log(className);
+
+        if(Object.keys(localStorage).includes(className)) {
+            let item = localStorage.getItem(className);
+            //console.log(item);
+            //console.log(JSON.stringify(element));
+            //console.log(Array.isArray(item));
+            if(!Array.isArray(item)) {
+                let recordArray = [];
+                recordArray.push(item);
+                recordArray.push(JSON.stringify(element));
+                localStorage.setItem(className, recordArray);
+            } else {
+                item.push(JSON.stringify(element));
+                localStorage.setItem(className, item);
+            }
+            
+        } else {
+            let recordArray = [];
+            recordArray.push(JSON.stringify(element));
+            localStorage.setItem(className, recordArray);
+        }
+        
+    });
+
+    console.log(localStorage.getItem('CS633SPRING2020'));
+
+    console.log(Object.keys(localStorage));
 }
 
 export {handleFileAsync}
